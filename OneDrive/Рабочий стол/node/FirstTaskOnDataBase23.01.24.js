7c161d6c3731ebf9f468d30732242833fdc06365
RyricoD321221 <145660524+RyricoD321221@users.noreply.github.com>
2024-01-22 19:38:23 +0500

    sdfsdf

---
+++ OneDrive/Рабочий стол/node/FirstTaskOnDataBase23.01.24.js
@@ -0,0 +1,11 @@
+const Students = [
+{
+     id: 1, FIO: "Yangirov Denis Fidanovich", birthday: "14-10-2005", id_Grouo: 1, Sex: "man",
+},
+    {
+        id: 2, FIO: "Artem Kicelev", birthday: "25-12.2006", id_Grouo: 1, Sex: "man",
+    }
+]
+
+console.log(Students[1].FIO);
+
